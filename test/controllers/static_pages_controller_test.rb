99147ccb8c201c5a5029207bbb095aea5cca14b6
mag67 <ec2-user@ip-172-31-39-65.ap-northeast-1.compute.internal>
2020-06-10 17:24:32 +0000

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,27 +12,32 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
   
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title","Help | #{@base_title}"
   end
   
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title","About | #{@base_title}"
   end
   
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title","Contact | #{@base_title}"
+  end
+  
+  test "full title helper" do
+    assert_equal full_title, "Ruby on Rails Tutorial Sample App"
+    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
   end
 
 end
